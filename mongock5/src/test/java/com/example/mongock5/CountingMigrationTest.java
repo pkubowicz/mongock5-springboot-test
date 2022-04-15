0d9c812db54cf642ac9541c65e36b9474841e2af
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2022-04-15 17:58:48 +0200

    Write the test using MongockStandalone runner

--- mongock5/src/test/java/com/example/mongock5/CountingMigrationTest.java
+++ mongock5/src/test/java/com/example/mongock5/CountingMigrationTest.java
@@ -1,8 +1,56 @@
 package com.example.mongock5;
 
-import com.example.mongock.BaseCountingMigrationTest;
+import com.example.mongock.MigrationExecutor;
+import com.example.testcontainers.MongoInitializer;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {MongockMigrationExecutor.class, NoMongockSpringConfiguration.class})
+@ContextConfiguration(initializers = MongoInitializer.class)
 @TestPropertySource(properties = {"spring.data.mongodb.database=mongock5-springboot"})
-public class CountingMigrationTest extends BaseCountingMigrationTest {
+public class CountingMigrationTest {
+
+    @Autowired
+    private MigrationExecutor migrationExecutor;
+
+    @Value("${mongock.change-log-repository-name}")
+    private String migrationCollectionName;
+
+    private MongoDatabase mongoDatabase;
+
+    @Autowired
+    private void initDatabase(MongoDatabaseFactory mongoDatabaseFactory) {
+        mongoDatabase = mongoDatabaseFactory.getMongoDatabase();
+    }
+
+    @Test
+    public void works() {
+        mongoDatabase.getCollection("user").deleteMany(new Document());
+        mongoDatabase.getCollection("user").insertMany(
+                Stream.of("alice", "bob", "chris")
+                        .map(name -> new Document().append("email", name + "@mail.com"))
+                        .collect(toList())
+        );
+        mongoDatabase.getCollection("summary").deleteMany(new Document());
+
+        System.out.println("RUNNING MIGRATIONS");
+        migrationExecutor.runAllMigrations(mongoDatabase, migrationCollectionName);
+
+        var found = mongoDatabase.getCollection("summary").find();
+        assertThat(found).hasSize(1);
+        //noinspection ConstantConditions
+        assertThat(found.first().getInteger("userCount")).isEqualTo(3);
+    }
 }
